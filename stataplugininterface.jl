62d9acc78ddbab1b909b620efd5a787f0d68fa3e
droodman <droodman@gmail.com>
2025-11-23 22:07:03 -0500

    Comment out draft code in stataplugininterface

--- stataplugininterface.jl
+++ stataplugininterface.jl
@@ -271,19 +271,19 @@ function st_local(mac::AbstractString, tosave::AbstractString)
     nothing
 end
 
-"""
-    st_local(mac::AbstractString)
+# """
+#     st_local(mac::AbstractString)
 
-Returns value of a Stata local macro named.
-ONLY for internal use, as it can only access the locals in the program calling `plugin call _julia`
-"""
-function st_local(mac::AbstractString)
-    rc = @ccall dllpath[]. Meta.parse(raw""" metadata!(df, note1, """From Consumer Reports with permission""", style=:note) """)(("_"*mac)::Cstring, tosave::Cstring)::Cint
-    rc!=0 && throw(rc)
-    mac ∉ Set(("___jlans","___jlcomplete")) &&
-        SF_macro_save("___jllocals", SF_macro_use("___jllocals") * " " * mac)  # add to Stata local "__jllocals"
-    nothing
-end
+# Returns value of a Stata local macro named.
+# ONLY for internal use, as it can only access the locals in the program calling `plugin call _julia`
+# """
+# function st_local(mac::AbstractString)
+#     rc = @ccall dllpath[]. Meta.parse(raw""" metadata!(df, note1, """From Consumer Reports with permission""", style=:note) """)(("_"*mac)::Cstring, tosave::Cstring)::Cint
+#     rc!=0 && throw(rc)
+#     mac ∉ Set(("___jlans","___jlcomplete")) &&
+#         SF_macro_save("___jllocals", SF_macro_use("___jllocals") * " " * mac)  # add to Stata local "__jllocals"
+#     nothing
+# end
 
 """
     st_matrix(matname::AbstractString)::Matrix{Float64}
